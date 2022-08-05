6f3dce67aa90e76d36bd38ac80e15a4616fd715b
Paulo Costa <me@paulo.costa.nom.br>
2022-08-05 04:56:41 -0300

    Much improved ping code

--- aionettools/ping.py
+++ aionettools/ping.py
@@ -1,158 +1,222 @@
-import os
+from dataclasses import dataclass, field
 import argparse
+from enum import Enum, auto
+from ipaddress import _BaseAddress, IPv4Address, IPv6Address
+import random
 import socket
 import asyncio
+import statistics
 import struct
+from typing import Any, List, Mapping, Optional, Tuple
+from asyncio import Future
 from timeit import default_timer as timer
+from socket import AddressFamily
+from typing_extensions import Self
 
-ICMP_ECHO_REQUEST = 8
-ICMP_ECHO_REPLY = 0
-ICMP6_ECHO_REQUEST = 128
-ICMP6_ECHO_REPLY = 129
+from .util import resolve_addresses
 
-def checksum(data):
-    sum = 0
-    for i in range(0, len(data), 2):
-        sum += data[i] * 256 + data[i + 1]       
-    if len(data) & 1:
-        sum += data[-1]
-    while sum >> 16:
-        sum = (sum & 0xffff) + (sum >> 16)
-    return ~sum & 0xffff
+@dataclass
+class PingResponse:
+    class Status(Enum):
+        PENDING = auto()
+        SUCESS = auto()
+        TIMEOUT = auto()
+        CANCELED = auto()
+        UNREACHABLE = auto()
 
+    addr: bytes
+    seq: int
+    payload: bytes
+    start: float = field(default_factory=timer)
+    end: Optional[float] = None
+    status: Status = Status.PENDING
+    _future: Future[Self] = field(default_factory=lambda: asyncio.get_event_loop().create_future())
 
-class IcmpProtocol:
-    pending_pings = {}
-    echo_id = os.getpid()
-    echo_seq = 0
+    @property
+    def key(self):
+        return (self.seq, self.payload)
+
+    @property
+    def elapsed(self) -> float:
+        if self.end is None:
+            return None
+        return self.end - self.start
+
+    def complete(self, status: Status):
+        if self.status == PingResponse.Status.PENDING:
+            self.status = status
+            self.end = timer()
+            self._future.set_result(self)
+
+class Ping:
+    ICMP_ECHO_REQUEST = 8
+    ICMP_ECHO_REPLY = 0
+    ICMP6_ECHO_REQUEST = 128
+    ICMP6_ECHO_REPLY = 129
+
+    def __init__(self) -> None:
+        self.echo_seq = 0
+        self.loop = asyncio.get_event_loop()
+        self.pending_pings: Mapping[int, PingResponse] = {}
+        self.sockets: Mapping[AddressFamily, socket.socket] = {}
+        self.pending_sends: Mapping[AddressFamily, List[Tuple[bytes, _BaseAddress]]] = {}
+
+        for family, protocol in [(AddressFamily.AF_INET, socket.getprotobyname("icmp")), (AddressFamily.AF_INET6, socket.getprotobyname("ipv6-icmp"))]:
+            sock = socket.socket(family, socket.SOCK_DGRAM, protocol)
+            self.sockets[family] = sock
+            self.pending_sends[family] = []
+            sock.setblocking(False)
+            self.loop.add_reader(sock.fileno(), self.recv_ready, sock)
     
-    @staticmethod
-    async def create(family):
-        if family == socket.AddressFamily.AF_INET:
-            sock = socket.socket(socket.AddressFamily.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
-        elif family == socket.AddressFamily.AF_INET6:
-            sock = socket.socket(socket.AddressFamily.AF_INET6, socket.SOCK_RAW, socket.getprotobyname("ipv6-icmp"))
-        sock.setblocking(False)
-        
-        protocol = IcmpProtocol()
-        loop = asyncio.get_event_loop()
-        waiter = loop.create_future()
-        
-        transport = loop._make_datagram_transport(
-            sock=sock, protocol=protocol, address=None, waiter=waiter)
-        
-        await waiter
-        return protocol
-
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        self.transport.close()
-    
-    def connection_made(self, transport):
-        self.transport = transport
-        self.family = transport._sock.family
+        for sock in self.sockets.values():
+            self.loop.remove_reader(sock)
+            self.loop.remove_writer(sock)
+            sock.close()
+        for pending_ping in self.pending_pings.values():
+            pending_ping.complete(PingResponse.Status.CANCELED)
 
-    def connection_lost(self, exc):
-        print("Connection closed")
-        self.transport = None
-        self.family = None
+    def recv_ready(self, sock: socket.socket):
+        data, address = sock.recvfrom(4096)
+        self.datagram_received(data, address)
+
+    def datagram_received(self, icmp_data, addr):
+        type, code, checksum = struct.unpack("!BBH", icmp_data[:4])
+        remaining = icmp_data[4:]
+
+        if type in [Ping.ICMP_ECHO_REPLY, Ping.ICMP6_ECHO_REPLY] and code == 0:
+            id, seq = struct.unpack("!HH", remaining[:4])
+            remaining = remaining[4:]
+            payload = remaining
+            #print(f"id={id}, seq={seq}, payload={payload}")
+            key = (seq, payload)
+            pending_ping = self.pending_pings.get(key)
+            if pending_ping and pending_ping.payload == payload:
+                pending_ping.complete(PingResponse.Status.SUCESS)
+        else:
+            #print(f"Discarding unexpected ICMP package: addr={addr}, type={type}, code={code}, remaining={remaining}")
+            pass
         
-    def error_received(self, exc):
-        print('Error received:', exc)
+    def send_ready(self, family: AddressFamily):
+        queue = self.pending_sends[family]
+        sock = self.sockets[family]
+        if len(queue) > 0:
+            data, address = queue.pop(0)
+            sock.sendto(data, (address.compressed, 0))
+        if len(queue) == 0:
+            self.loop.remove_writer(sock.fileno())
         
-    def datagram_received(self, data, addr):
-        #print('Received:', data, addr)
+    def ping(self, addr: _BaseAddress, timeout: Optional[float] = 1, **kwargs):
+        seq = self.echo_seq
+        self.echo_seq = (self.echo_seq + 1) & 0xFFFF
+
+        if isinstance(addr, IPv4Address):
+            family = AddressFamily.AF_INET
+            icmp_type = Ping.ICMP_ECHO_REQUEST
+        elif isinstance(addr, IPv6Address):
+            family = AddressFamily.AF_INET6
+            icmp_type = Ping.ICMP6_ECHO_REQUEST
+        else:
+            raise ValueError("Unexpected address type: {addr} -- Should be IPv4Address or IPv6Address")
         
-        if self.family == socket.AddressFamily.AF_INET:
-            ip_data = data[0:20]
-            icmp_data = data[20:]
-        elif self.family == socket.AddressFamily.AF_INET6:
-            ip_data = None
-            icmp_data = data
+        header = struct.pack(
+            "!BBHHH", 
+            icmp_type, # ICMP Type: ECHO_REQUEST
+            0, # ICMP Code: always zero for ping
+            0, # Checksum -- Populated later by the linux kernel
+            0, # Identifier -- Populated later by the linux kernel
+            seq, # Sequence number
+        )
+        payload = bytes([random.getrandbits(8) for i in range(10)])
+        packet = header + payload
         
-        #assert 0 == checksum(icmp_data), f"Invalid checksum: {checksum(icmp_data)}"
-        
-        type = icmp_data[0]
-        code = icmp_data[1]
-        #rest_of_header = icmp_data[4:8]
-        #data = icmp_data[8:]
-        
-        #print(f"Received ICMP package: addr={addr}, type={type}, code={code}")
-        
-        if (self.family == socket.AddressFamily.AF_INET and type == ICMP_ECHO_REPLY) or (self.family == socket.AddressFamily.AF_INET6 and type == ICMP6_ECHO_REPLY):
-            id_seq = icmp_data[4:8]
-            data = icmp_data[8:]
-            request = IcmpProtocol.pending_pings.pop(id_seq)
-            if request:
-                request.set_result(data)
-        
-    def send_icmp(self, addr, type, code, data):
-        if self.family == socket.AddressFamily.AF_INET:
-            assert len(data) >= 4, f"ICMP data should have at least 4 bytes"
-            
-        #print(f"type={type}, code={code}, rest_of-header={rest_of_header}, data={data}")
-        header = struct.pack("!BBH", type, code,  0)
-        header = struct.pack("!BBH", type, code, checksum(header + data))
-        
-        packet = header + data
-        assert 0 == checksum(packet), f"Invalid checksum: {checksum(packet)}"
-        
-        self.transport.sendto(packet, addr)
-        
-        
-    async def ping(self, addr, data=None, timeout=None):
-        IcmpProtocol.echo_seq += 1
-        #print(IcmpProtocol.echo_id, IcmpProtocol.echo_seq)
-        id_seq = struct.pack("!HH", IcmpProtocol.echo_id & 0xFFFF, IcmpProtocol.echo_seq & 0xFFFF)
-        if self.family == socket.AddressFamily.AF_INET:
-            icmp_type = ICMP_ECHO_REQUEST
-        elif self.family == socket.AddressFamily.AF_INET6:
-            icmp_type = ICMP6_ECHO_REQUEST
-        
-        if data is None:
-            data = b"abcdef"#bytes([random.getrandbits(8) for i in range(10)])
-            
-        loop = asyncio.get_event_loop()
-        waiter = loop.create_future()
-        
-        
-        IcmpProtocol.pending_pings[id_seq] = waiter
-        try:
-            start = timer()
-            self.send_icmp(
-                addr=addr,
-                type=icmp_type,
-                code=0,
-                data=id_seq + data)
-            
-            await asyncio.wait_for(waiter, timeout)
-            end = timer()
-            return 1000 * (end - start)
-        finally:
-            if IcmpProtocol.pending_pings.get(id_seq, None) == waiter:
-                del IcmpProtocol.pending_pings[id_seq]
- 
+        pending_ping = PingResponse(addr=addr, seq=seq, payload=payload)
+        for key, value in kwargs.items():
+            if hasattr(pending_ping, key):
+                raise ValueError(f"Cannot specify reserver field '{key}'")
+            setattr(pending_ping, key, value)
+
+        self.pending_pings[pending_ping.key] = pending_ping
+        sock = self.sockets[family]
+        self.pending_sends[family].append((packet, addr))
+        if len(self.pending_sends[family]) == 1:
+            self.loop.add_writer(sock.fileno(), self.send_ready, family)
+
+        ret = pending_ping._future
+        ret.add_done_callback(lambda _: self.pending_pings.pop(pending_ping.key))
+        if timeout is not None:
+            loop = asyncio.get_event_loop()
+            def on_timeout():
+                pending_ping.complete(PingResponse.Status.TIMEOUT)
+            timeout_handle = loop.call_later(timeout, on_timeout)
+            ret.add_done_callback(lambda _: timeout_handle.cancel())
+        return ret
+
+    async def wait_pending(self):
+        await asyncio.gather(*[pending.future for pending in self.pending_pings.values()], return_exceptions=True)
+
 async def main():
     parser = argparse.ArgumentParser(description='Ping')
-    parser.add_argument('host', metavar='HOST', nargs='?', default="example.com", help="Host to ping")
+    parser.add_argument('hostname', metavar='HOST', nargs='?', default="example.com", help="Host to ping")
+    parser.add_argument('--count', '-c', metavar='COUNT', type=int, default=None, help="Stop after sending COUNT ECHO_REQUEST packets")
+    parser.add_argument('--interval', '-i', metavar='INTERVAL', type=float, default=None, help="Wait INTERVAL seconds between sending each packet")
+    parser.add_argument('--timeout', '-W', metavar='TIMEOUT', type=float, default=1, help="Time to wait for a response, in seconds")
+    parser.add_argument('--flood', '-f', action='store_true', help="Wait INTERVAL seconds between sending each packet")
+    parser.add_argument('--audible', '-a', action='store_true', help="Audible ping")
     args = parser.parse_args()
     
-    loop = asyncio.get_event_loop()
-    addr = (await loop.getaddrinfo(args.host, 0))[0]
-    family = addr[0]
-    addr = addr[4]
-    print(f"addr: {addr}, family={repr(family)}")
-    async with await IcmpProtocol.create(family) as icmp:
-        while True:
-            try:
-                print(f"Pong: {await icmp.ping(addr, timeout=1):.1f}ms")
-            except asyncio.CancelledError:
-                raise
-            except:
-                print(f"Pong failed")
-            break
+    interval = args.interval if args.interval is not None else 0.005 if args.flood else 1
+    addresses = await resolve_addresses(args.hostname)
+
+    ping_count = 0
+    pongs = []
+    try:
+        async with Ping() as ping:
+            def pong_callback(future: Future[PingResponse]):
+                result = future.result()
+
+                if result.status == PingResponse.Status.SUCESS:
+                    pongs.append(result.elapsed)
+                    if args.audible:
+                        print("\a", end="", flush=True)
+                    if args.flood:
+                        print("\b \b", end="", flush=True)
+
+                if not args.flood:
+                    print(f"{result.hostname} ({result.addr}): icmp_seq={result.seq}, time={result.elapsed * 1000:.1f} ms, {result.status.name}")
+
+            while args.count is None or ping_count < args.count:
+                if ping_count > 0:
+                    await asyncio.sleep(interval)
+                ping_count += 1
+                if args.flood:
+                    print(".", end="", flush=True)
+
+                addresses = addresses[1:] + addresses[:1]
+                addr = addresses[0]
+                pong = ping.ping(addr, timeout=args.timeout, hostname=args.hostname)
+                pong.add_done_callback(pong_callback)
+
+            await ping.wait_pending()
+    finally:
+        if ping_count > 0:
+            success_rate = len(pongs) / ping_count
+            if pongs:
+                time_mean = statistics.mean(pongs)
+                time_stderr = statistics.stdev(pongs)
+                latency = f"{1000 * time_mean :.1f} ± {1000 * time_stderr :.1f} ms"
+            else:
+                latency = "N/A"
+            if args.flood:
+                print()
+            print(f"Success Rate: {100 * success_rate : .2f} %, latency={latency}")
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        ...
